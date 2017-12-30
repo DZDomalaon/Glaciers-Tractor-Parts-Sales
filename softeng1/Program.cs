d46b227014aa11f51971663a51ea43b43a059c5d
Darell Domalaon <darelldomalaon@gmail.com>
2017-12-30 18:14:07 +0800

    changed program.cs

--- softeng1/Program.cs
+++ softeng1/Program.cs
@@ -16,7 +16,7 @@ namespace softeng1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new orderForm());
+            Application.Run(new loginForm());
         }
     }
 }
