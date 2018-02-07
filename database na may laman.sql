ff88abf54c1be6328362fe180fa6c4229a9046b3
Darell Domalaon <darelldomalaon@gmail.com>
2018-02-07 08:52:46 +0800

    update db

--- database na may laman.sql
+++ database na may laman.sql
@@ -3,6 +3,8 @@
 -- Host: localhost    Database: glaciers
 -- ------------------------------------------------------
 -- Server version	5.7.19-log
+create database glaciers;
+use glaciers;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
