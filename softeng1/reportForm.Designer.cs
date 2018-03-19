769b0ce338082a25ae0139105fd632a00102f2ed
jtsbellezas <33455812+jtsbellezas@users.noreply.github.com>
2018-03-19 21:31:18 +0800

    update na ambot tabang

--- softeng1/reportForm.Designer.cs
+++ softeng1/reportForm.Designer.cs
@@ -30,16 +30,18 @@
         {
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.SalesReport = new softeng1.SalesReport();
+            this.SalesReport1 = new softeng1.SalesReport();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.ActiveViewIndex = 0;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.SalesReport1;
             this.crystalReportViewer1.Size = new System.Drawing.Size(622, 314);
             this.crystalReportViewer1.TabIndex = 0;
             // 
@@ -60,5 +62,6 @@
 
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
         private SalesReport SalesReport;
+        private SalesReport SalesReport1;
     }
 }
